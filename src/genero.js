d8d5f822ee968f3acce91fc3afbd9993714f4b3c
Chowfie <93269740+chowfie@users.noreply.github.com>
2022-11-22 17:08:19 +0100

    #4 Desarrollo implementación clase entidad Género

---
+++ src/genero.js
@@ -0,0 +1,16 @@
+/**
+  * Declaración clase Género
+ */
+
+class Genero
+{
+  /**
+  *  @param {String} nombre //Nombre del género musical
+  *  @param {int} oyentes //Número de veces escuchado
+  */
+  
+  constructor(nombre, oyentes){
+    this.nombre = nombre;
+    this.oyentes = oyentes;
+  }
+}
