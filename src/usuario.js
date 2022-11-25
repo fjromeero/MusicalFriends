0f2257e411d7af801f4c06c25d411f159605096f
Sofia Real Lucas <93269740+chowfie@users.noreply.github.com>
2022-11-25 18:10:41 +0100

    #8 Corrección desarrollo e implementación Usuario

--- src/usuario.js
+++ src/usuario.js
@@ -1,28 +1,9 @@
-/**
-  * Declaración clase Usuario
- */
+/* Declaración clase Usuario */
 
 class Usuario
 {
-  /**
-  *  @param {String} nombre //Nombre del usuario
-  *  @param {String} apellidos //Apellidos del usuario
-  *  @param {String} nickname //Nickname del usuario
-  *  @param {int} edad 
-  *  @param {String} ciudad
-  *  @param {ArrayList{Genero}} generos//Generos escuchados por el usuario
-  */
-  
-    constructor(nombre, apellidos, nickname, edad, ciudad, generos){
-      this.nombre = nombre;
-      this.apellidos = apellidos;
+    constructor(nickname, generos){
       this.nickname = nickname;
-      this.edad = edad;
-      this.ciudad = ciudad;
-
-      /**
-        * No se si inicializar a nulo el array de generos escuchados, ya que no se como se añaden estos
-        * Otra duda es si es mejor que la clase usuario guarden los generos o los generos guarden a los usuarios que los escuchan
-      */
+      this.generos = generos;
     }
 }
