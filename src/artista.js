d9a28ed26850e3e3e2cd0f28ec323d46d6a09316
fjromeero <franjaviro2001@gmail.com>
2022-12-22 23:53:23 +0100

    Closes #18 añade clase que modeliza artistas Con la modelización de los artistas podremos incluir los géneros que caracterizan a estos y así tener los géneros escuchados por el usuario

---
+++ src/artista.js
@@ -0,0 +1,8 @@
+/* Clase que representa a un artista incluyendo su nombre y los generos que le representa */
+
+class Artista{
+    constructor(nombre, generos){
+        this.nombre = nombre
+        this.generos = generos
+    }
+}
