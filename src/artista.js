2ccbd5dfc46d067684094e263d4e0d7a26376e35
fjromeero <franjaviro2001@gmail.com>
2022-12-23 01:05:39 +0100

    Close #17 añade clase que modulariza los usuarios Se ha implementado una clase que modulariza los usuarios con un constructor que procesa un json con la información de los artistas más escuchados por este.

--- src/artista.js
+++ src/artista.js
@@ -1,6 +1,6 @@
 /* Clase que representa a un artista incluyendo su nombre y los generos que le representa */
 
-class Artista{
+export class Artista{
     constructor(nombre, generos){
         this.nombre = nombre
         this.generos = generos
--- src/usuario.js
+++ src/usuario.js
@@ -1,9 +1,24 @@
+import { Artista } from "./artista.js";
 /* Declaración clase Usuario */
 
-class Usuario
-{
-    constructor(nickname, generos){
-      this.nickname = nickname;
-      this.generos = generos;
+class Usuario {
+  constructor(nickname, json_artistas) {
+    this.nickname = nickname;
+    this.artistas = [];
+    if(json_artistas['items']){
+      for (var i = 0; i < json_artistas['items'].length; i++) {
+        if (json_artistas['items'][i].type != "artist") {
+          throw("Error: el archivo no es de artistas")
+        } else {
+          let generos = [];
+          for (var n_genero = 0; n_genero < json_artistas['items'][i].genres.length; n_genero++) {
+            generos.push(json_artistas['items'][i].genres[n_genero])
+          }
+          this.artistas.push(new Artista(json_artistas['items'][i].name, generos))
+        }
+      }
+    }else{
+      throw("Error: el archivo de artistas no es válido")
     }
+  }
 }
