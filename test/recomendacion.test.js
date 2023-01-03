78307a4609c0d1e21b2ae01406237dd72342e9bc
fjromeero <franjaviro2001@gmail.com>
2023-01-03 19:12:57 +0100

    Refactoriza tests debido a los cambios anteriores
    Signed-off-by: fjromeero <franjaviro2001@gmail.com>

--- test/recomendacion.test.js
+++ test/recomendacion.test.js
@@ -1,23 +1,20 @@
-const Recomendador = require('../src/recomendador')
-const Usuario = require('../src/usuario')
+const {Usuario, fromJSONToUsuario, recomendarUsuarios} = require('../src/usuario')
 const datos_fran = require('../data/fran.json')
 const datos_pepe = require('../data/pepe.json')
 const datos_angela = require('../data/angela.json')
 
 describe("Comprobación del recomendador", () => {
-    const fran = new Usuario('Fran',datos_fran) 
-    const pepe = new Usuario('Pepe',datos_pepe)
-    const angela = new Usuario('Angela', datos_angela)
+    const fran = fromJSONToUsuario('Fran', datos_fran)
+    const pepe = fromJSONToUsuario('Pepe',datos_pepe)
+    const angela = fromJSONToUsuario('Angela', datos_angela)
 
+    const usuarios = [fran,pepe,angela]
 
-    const recomendador = new Recomendador([fran, pepe, angela])
+    const recomendados_fran = recomendarUsuarios(usuarios,fran)
+    const recomendados_pepe = recomendarUsuarios(usuarios,pepe)
+    const recomendados_angela = recomendarUsuarios(usuarios,angela)
 
-    const recomendados_fran = recomendador.recomendarUsuarios(fran)
-    const recomendados_pepe = recomendador.recomendarUsuarios(pepe)
-    const recomendados_angela = recomendador.recomendarUsuarios(angela)
-
-    it("Constructor: ", () => {
-        expect(recomendador.usuarios).toStrictEqual([fran, pepe, angela])
+    it("Recomendaciones: ", () => {
         expect(recomendados_fran).toStrictEqual([pepe])
         expect(recomendados_pepe).toStrictEqual([fran,angela])
         expect(recomendados_angela).toStrictEqual([pepe])
--- test/usuario.test.js
+++ test/usuario.test.js
@@ -1,25 +1,22 @@
-const Usuario = require('../src/usuario')
+const {Usuario, fromJSONToUsuario} = require('../src/usuario')
 const datos_fran = require('../data/fran.json')
 const datos_erroneos = require('../data/erroneo.json')
 
 describe("Comprobación de los usuarios", () => {
-    const fran = new Usuario("Fran", datos_fran);
+    const fran = fromJSONToUsuario('Fran', datos_fran)
     
-    it("Operaciones básicas del constructor",() => {
+    it("Operaciones básicas del constructor y el procesamiento del JSON",() => {
         expect(fran.nickname).toBe("Fran");
         expect(fran.top_artistas[0].nombre).toBe("Echos");
         expect(fran.top_artistas[0].generos[0]).toBe("alt z");
         expect(fran.top_artistas[0].generos[1]).toBe("indie poptimism");
         expect(fran.top_artistas[9].nombre).toBe("Kidd Keo");
+        expect(fran.generos_favoritos).toStrictEqual(["gym hardstyle"])
     })
 
     it("Excepciones en el constructor", () => {
         expect(
-            () => new Usuario("Error", datos_erroneos)
+            () => fromJSONToUsuario("Error", datos_erroneos)
         ).toThrowError("Error: el archivo no es de artistas")
-    })
-
-    it("Funcionamiento calculo de los géneros favoritos", () => {
-        expect(fran.generos_favoritos).toStrictEqual(["gym hardstyle"])
     })
 })
