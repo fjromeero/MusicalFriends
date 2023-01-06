64ab9d6e6c1f539f47e7e142d53b5887f2996a22
fjromeero <franjaviro2001@gmail.com>
2023-01-06 19:44:17 +0100

    Avanza #22 testeando que se logra #28
    Signed-off-by: fjromeero <franjaviro2001@gmail.com>

--- test/usuario.test.js
+++ test/usuario.test.js
@@ -2,6 +2,7 @@ const {fromJSONToUsuario} = require('../src/usuario')
 
 const datos_fran = require('../data/fran.json')
 const datos_pepe = require('../data/pepe.json')
+const erroneo = require('../data/erroneo.json')
 
 describe("Comprobacion de la creacion de un usuario" ,() =>{
     const fran = fromJSONToUsuario('Fran',datos_fran)
@@ -10,5 +11,13 @@ describe("Comprobacion de la creacion de un usuario" ,() =>{
     it("Procesamiento de datos en JSON y calculo de generos favoritos",() => {
         expect(fran.generos_favoritos).toStrictEqual(['gym hardstyle'])
         expect(pepe.generos_favoritos).toStrictEqual(['urbano latino','gym hardstyle'])
+    })
+})
+
+describe("Comprobacion de lanzamiento de excepciones", () => {
+    it("Lanzamiento de excepcion cuando se intenta crear archivo a partir de un archivo erroneo", () => {
+        expect(
+            () => fromJSONToUsuario('',erroneo)
+        ).toThrowError("Error: el archivo no es de artistas")
     })
 })
