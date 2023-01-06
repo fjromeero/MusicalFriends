a73141001dcd4f32da8205976816f4947fc6b1c2
fjromeero <franjaviro2001@gmail.com>
2023-01-06 01:27:40 +0100

    Close #21 crea funcion calculaGenerosFavoritos
    Signed-off-by: fjromeero <franjaviro2001@gmail.com>

--- src/usuario.js
+++ src/usuario.js
@@ -4,3 +4,24 @@ class Usuario{
         this.generos_favoritos = generos_favoritos
     }
 }
+
+function calculaGenerosFavoritos(generos){
+    const frecuencia_generos = {}
+    generos.map(genero => {
+        if(genero in frecuencia_generos){
+            frecuencia_generos[genero] +=1
+        }else{
+            frecuencia_generos[genero] = 1
+        }
+    })
+    
+    const frecuencia_maxima = Math.max(...Object.values(frecuencia_generos))
+    const generos_favoritos = []
+
+    Object.keys(frecuencia_generos).map(genero => {
+        frecuencia_generos[genero] === frecuencia_maxima ? generos_favoritos.push(genero) : null
+    })
+
+    const usuario = new Usuario(nickname,generos_favoritos)
+    return usuario
+}
